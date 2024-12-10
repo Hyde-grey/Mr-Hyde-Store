85fe78e2a34b1d22573c16ee9bc038fa76dcff8b
Hyde <muzika.senpai@gmail.com>
2024-12-10 12:33:58 +0000

    feat: change initial camera position and disable camera span on mobile

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,15 +3,25 @@ import { OrbitControls, ScrollControls, Stars } from "@react-three/drei";
 import HomeLayout from "./homeLayout";
 import "./home.css";
 import Spiral from "../../components/Spiral";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    if (/android|iPad|iPhone|iPod/i.test(userAgent)) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <>
-      <Canvas camera={{ position: [15, 1, 5], fov: 40 }}>
+      <Canvas camera={{ position: [17, 4, 0], fov: 35 }}>
         <ambientLight intensity={5} />
         <directionalLight position={[30, 35, 5]} intensity={2} />
         <pointLight position={[0, -10, -10]} intensity={50} />
-        <OrbitControls enableZoom={false} />
+        <OrbitControls enableZoom={false} enabled={!isMobile} />
         <Stars />
         <ScrollControls pages={4}>
           <Spiral />
