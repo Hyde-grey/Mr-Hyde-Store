2d19575fdbaa4183d06fc7cc39eef12b8ce7be2d
Hyde <muzika.senpai@gmail.com>
2024-12-13 23:59:17 +0000

    feat: add burger to cross animation on mobile bottom menu

--- src/components/navigationBar/BottomNav.tsx
+++ src/components/navigationBar/BottomNav.tsx
@@ -1,12 +1,22 @@
 import { useScrollContext } from "../../contexts/ScrollContext";
 import classNames from "classnames";
 import styles from "./BottomNav.module.css";
+import { useState } from "react";
 
 const BottomNav = () => {
   const { scrollOffset } = useScrollContext();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const burgerClickHandler = () => {
+    setIsClicked(!isClicked);
+    console.log("is clicked", isClicked);
+  };
 
   return (
     <div
+      onClick={() => {
+        burgerClickHandler();
+      }}
       className={classNames(styles.scrollProgress)}
       style={{
         background: `conic-gradient(from 0deg, rgb(206, 206, 206) ${
@@ -17,15 +27,25 @@ const BottomNav = () => {
       <div className={classNames(styles.bottomNavContainer)}>
         <ul className={classNames(styles.bottomNavBar)}>
           <li>
-            <div className={classNames(styles.burgerContainer)}>
+            <div
+              className={classNames(styles.burgerContainer, {
+                [styles.burgerClicked]: isClicked,
+              })}
+            >
               <div
-                className={classNames(styles.burgerLines, styles.topLine)}
+                className={classNames(styles.burgerLines, styles.topLine, {
+                  [styles.topLineClicked]: isClicked,
+                })}
               ></div>
               <div
-                className={classNames(styles.burgerLines, styles.middleLine)}
+                className={classNames(styles.burgerLines, styles.middleLine, {
+                  [styles.middleLineClicked]: isClicked,
+                })}
               ></div>
               <div
-                className={classNames(styles.burgerLines, styles.bottomLine)}
+                className={classNames(styles.burgerLines, styles.bottomLine, {
+                  [styles.bottomLineClicked]: isClicked,
+                })}
               ></div>
             </div>
           </li>
