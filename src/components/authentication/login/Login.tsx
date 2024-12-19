41517ba04a1a18d584cf4c3e09b94cccdb968ef9
Hyde <muzika.senpai@gmail.com>
2024-12-19 17:43:40 +0000

    feat: change login to use FormLayout and buttonLayout components

--- src/components/authentication/login/Login.tsx
+++ src/components/authentication/login/Login.tsx
@@ -1,15 +1,23 @@
+import React from "react";
+import FormLayout from "../../forms/FormLayout";
+import ButtonLayout from "../../button/Button";
+
 const LoginForm = () => {
+  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("this form is being submitted");
+  };
+
   return (
-    <div>
-      <div>
-        <h2>already have an account ?</h2>
-        <p>Signin with you email and password</p>
-      </div>
-      <form action="">
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-      </form>
-    </div>
+    <FormLayout
+      title="Already have an account ?"
+      text="Signin with your email and password"
+      formSubmitHandler={formSubmitHandler}
+    >
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <ButtonLayout buttonType="submit">Login</ButtonLayout>
+    </FormLayout>
   );
 };
 
