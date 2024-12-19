417492453e7a1192454aba2bb6d5864046b31b23
Hyde <muzika.senpai@gmail.com>
2024-12-19 17:41:39 +0000

    feat: create FormLayout for reusability

---
+++ src/components/forms/FormLayout.tsx
@@ -0,0 +1,34 @@
+import classNames from "classnames";
+import styles from "./FormLayout.module.css";
+import React from "react";
+
+type FormLayoutProps = {
+  title: string;
+  text: string;
+  children: React.ReactNode;
+  formSubmitHandler: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const FormLayout = ({
+  title,
+  text,
+  children,
+  formSubmitHandler,
+}: FormLayoutProps) => {
+  return (
+    <div className={classNames(styles.formContainer)}>
+      <div className={classNames(styles.formTitleContainer)}>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </div>
+      <form
+        className={classNames(styles.formField)}
+        onSubmit={formSubmitHandler}
+      >
+        {children}
+      </form>
+    </div>
+  );
+};
+
+export default FormLayout;
