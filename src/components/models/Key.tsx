d6192923f617f434d8ebabe69751155d207a82dc
Hyde <muzika.senpai@gmail.com>
2024-12-19 17:45:43 +0000

    feat: add keyModel to authpage

---
+++ src/components/models/Key.tsx
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+import { useGLTF, useScroll } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Key_2: THREE.Mesh;
+  };
+  materials: {
+    ["silver mat"]: THREE.MeshStandardMaterial;
+  };
+};
+
+export const KeyModel = (props: JSX.IntrinsicElements["group"]) => {
+  const { nodes, materials } = useGLTF(
+    "public/KeyModel/KeyAsset.gltf"
+  ) as GLTFResult;
+
+  const ref = useRef<THREE.Mesh>(null);
+  const scroll = useScroll();
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.z = initialRotation.z + scroll.offset * Math.PI * 4;
+    }
+  });
+
+  const initialRotation = { x: 0.5, y: 0, z: 2 };
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        ref={ref}
+        scale={2.5}
+        geometry={nodes.Key_2.geometry}
+        material={materials["silver mat"]}
+        position={[-0.016, 0, -0.238]}
+        rotation={[initialRotation.x, initialRotation.y, initialRotation.z]}
+      />
+    </group>
+  );
+};
+
+useGLTF.preload("/KeyAsset.gltf");
--- src/pages/authPage/AuthPage.tsx
+++ src/pages/authPage/AuthPage.tsx
@@ -1,15 +1,17 @@
 import { Scroll } from "@react-three/drei";
 import classNames from "classnames";
 
-import MainCanvas from "../../components/MainCanvas/MainCanvas";
+import MainCanvas from "../../components/mainCanvas/MainCanvas";
 import LoginForm from "../../components/authentication/login/Login";
 import SignUp from "../../components/authentication/signup/Signup";
 
 import styles from "./AuthStyles.module.css";
+import { KeyModel } from "../../components/models/Key";
 
 const AuthPage = () => {
   return (
     <MainCanvas numberOfPages={1}>
+      <KeyModel />
       <Scroll html>
         <div className={classNames(styles.authPageContainer)}>
           <LoginForm />
