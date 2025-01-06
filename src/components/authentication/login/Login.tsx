f04c0b03259045bfe24aade5c1b54a5d69154c5a
Hyde <muzika.senpai@gmail.com>
2025-01-06 20:20:13 +0000

    feat: create login component

--- src/components/authentication/login/Login.tsx
+++ src/components/authentication/login/Login.tsx
@@ -1,22 +1,68 @@
-import React from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import FormLayout from "../../forms/FormLayout";
 import ButtonLayout from "../../button/Button";
+import InputLayout from "../../forms/inputs/Inputs";
+import styles from "../../forms/FormLayout.module.css";
+
+type FormFields = {
+  email: string;
+  password: string;
+};
+
+const defaultFormFields: FormFields = {
+  email: "",
+  password: "",
+};
 
 const LoginForm = () => {
-  const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+  const [formFields, setFormFields] = useState<FormFields>(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const formSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("this form is being submitted");
+
+    try {
+      console.log("this form is being submitted");
+      resetFormFields();
+    } catch (error) {
+      console.log("user sign in failed", error);
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
   };
 
   return (
     <FormLayout
       title="Already have an account ?"
-      text="Signin with your email and password"
+      text="Sign in with your email and password"
       formSubmitHandler={formSubmitHandler}
     >
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <ButtonLayout buttonType="submit">Login</ButtonLayout>
+      <div className={styles.inputContainer}>
+        <InputLayout
+          type="text"
+          placeholder="Email"
+          onChange={handleChange}
+          value={email}
+          name="email"
+        />
+
+        <InputLayout
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          value={password}
+          name="password"
+        />
+        <ButtonLayout buttonType="submit">Login</ButtonLayout>
+      </div>
     </FormLayout>
   );
 };
