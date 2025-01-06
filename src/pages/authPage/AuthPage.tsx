1801bb61a7309026f63157ce439c3d8d925b20d2
Hyde <muzika.senpai@gmail.com>
2025-01-06 20:17:51 +0000

    feat: create auth page

--- src/pages/authPage/AuthPage.tsx
+++ src/pages/authPage/AuthPage.tsx
@@ -7,15 +7,55 @@ import SignUp from "../../components/authentication/signup/Signup";
 
 import styles from "./AuthStyles.module.css";
 import { KeyModel } from "../../components/models/Key";
+import { useState } from "react";
 
 const AuthPage = () => {
+  const [activeTab, setActiveTab] = useState("login");
+
+  const toggleTabHandler = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
     <MainCanvas numberOfPages={1}>
       <KeyModel />
       <Scroll html>
-        <div className={classNames(styles.authPageContainer)}>
-          <LoginForm />
-          <SignUp />
+        <div className={styles.authPageContainer}>
+          <div className={styles.authComponentContainer}>
+            <div className={styles.authSelectorsContainer}>
+              <div>
+                <div
+                  className={classNames(styles.authSelector, {
+                    [styles.activeTab]: activeTab === "login",
+                  })}
+                  onClick={() => toggleTabHandler("login")}
+                >
+                  Login
+                </div>
+                <div
+                  className={classNames(styles.linkBorder, {
+                    [styles.activelinkBorder]: activeTab === "login",
+                  })}
+                ></div>
+              </div>
+              <div>
+                <div
+                  className={classNames(styles.authSelector, {
+                    [styles.activeTab]: activeTab === "signup",
+                  })}
+                  onClick={() => toggleTabHandler("signup")}
+                >
+                  Sign Up
+                </div>
+                <div
+                  className={classNames(styles.linkBorder, {
+                    [styles.activelinkBorder]: activeTab === "signup",
+                  })}
+                ></div>
+              </div>
+            </div>
+            {activeTab === "login" ? <LoginForm /> : <SignUp />}
+          </div>
         </div>
       </Scroll>
     </MainCanvas>
