456a80658a7ed1163dd77cc9765ee23267f64244
Hyde <muzika.senpai@gmail.com>
2025-01-20 13:23:06 +0000

    update: userContext to properly handle setCurrentUser onAuthChanged

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import { User } from "firebase/auth";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState, ReactNode } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
 
 type UserContextType = {
   currentUser: User | null;
@@ -20,17 +20,17 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
       setCurrentUser(user);
     });
-
     return () => unsubscribe();
   }, []);
 
   const value = {
     currentUser,
-    setCurrentUser,
+    setCurrentUser: (user: User | null) => {
+      setCurrentUser(user);
+    },
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { UserProvider } from "./contexts/user.context.tsx";
+import { UserProvider } from "./contexts/UserContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
