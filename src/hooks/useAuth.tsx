7f261d7cea465a7c60c40b52162ee5e00166c325
Hyde <muzika.senpai@gmail.com>
2025-01-20 13:27:12 +0000

    update: useAuth to have getUserID and get userData functions

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,7 +3,36 @@ import {
   onAuthStateChanged,
   User as FirebaseUser,
 } from "firebase/auth";
+import { getDoc, doc } from "firebase/firestore";
 import { useState, useEffect } from "react";
+import { auth, db } from "../firebase/firebaseConfig";
+
+export const getUserId = async () => {
+  const user = auth.currentUser;
+  if (user) {
+    return user.uid;
+  } else {
+    console.log("No user is signed in.");
+    return null;
+  }
+};
+
+export const getUserData = async () => {
+  const userID = await getUserId();
+  if (userID) {
+    const docRef = doc(db, "users", userID);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      return docSnap.data();
+    } else {
+      console.log("No such document!");
+    }
+  } else {
+    console.log("No user is signed in.");
+  }
+};
 
 export const useAuth = () => {
   const [user, setUser] = useState<FirebaseUser | null>(null);
