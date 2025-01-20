97c6b33c7a6a367775e1e1928b0c6a288916231b
Hyde <muzika.senpai@gmail.com>
2025-01-20 13:30:29 +0000

    update: useLogout to navigate to home on logout

--- src/hooks/useLogOut.tsx
+++ src/hooks/useLogOut.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const useLogout = () => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const logout = async () => {
     setLoading(true);
@@ -16,6 +18,7 @@ export const useLogout = () => {
       setError(err.message);
     } finally {
       setLoading(false);
+      navigate("/");
     }
   };
 
