67d246779982a94e97c517d0c4859445a462f902
Hyde <muzika.senpai@gmail.com>
2025-01-27 12:22:21 +0000

    feat: create shopCollectionSection

---
+++ src/components/shopCollectionSection/shopCollectionsection.tsx
@@ -0,0 +1,28 @@
+import { Collection } from "../../hooks/useGetCollections";
+import ProductCard from "../productCard/productCard";
+import styles from "./shopCollectionsection.module.css";
+
+const ShopCollectionSection = ({
+  collections,
+}: {
+  collections: Collection[];
+}) => {
+  return (
+    <>
+      {collections.map(({ name, products }, index) => (
+        <div className={styles.collectionSection} key={index}>
+          <div className={styles.collectionTitle}>
+            <h2>{name}</h2>
+            <div className={styles.collectionDivider}></div>
+          </div>
+          <div className={styles.productsContainer}>
+            {products.map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+export default ShopCollectionSection;
