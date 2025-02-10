1b42b83b40a88f5b5600c1042529d36d880ce003
Hyde <muzika.senpai@gmail.com>
2025-02-10 17:54:01 +0000

    feat: wrap app in cartProvider

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { UserProvider } from "./contexts/UserContext";
 import { MenuProvider } from "./contexts/MenuContext.tsx";
 import { ScrollProvider } from "./contexts/ScrollContext.tsx";
 import { FavoritesProvider } from "./contexts/FavoritesContext";
-// import { CartProvider } from "./contexts/CartContext";
+import { CartProvider } from "./contexts/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,9 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <MenuProvider>
         <ScrollProvider>
           <FavoritesProvider>
-            {/* <CartProvider> */}
-            <App />
-            {/* </CartProvider> */}
+            <CartProvider>
+              <App />
+            </CartProvider>
           </FavoritesProvider>
         </ScrollProvider>
       </MenuProvider>
