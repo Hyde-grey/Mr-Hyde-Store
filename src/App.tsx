04788c552453ca6d800877522cacc42ca40d520c
Hyde <muzika.senpai@gmail.com>
2025-02-10 12:06:15 +0000

    feat: remove providers from App.tsx and place them in main.tsx instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesConfig from "./routes";
-import { ScrollProvider } from "../src/contexts/ScrollContext";
-import { MenuProvider } from "../src/contexts/MenuContext";
+
 import classNames from "classnames";
 import styles from "./components/MainCanvas/MainCanvas.module.css";
 
 const App = () => (
-  <MenuProvider>
-    <ScrollProvider>
-      <div className={classNames(styles.mainContainer)}>
-        <BrowserRouter>
-          <RoutesConfig />
-        </BrowserRouter>
-      </div>
-    </ScrollProvider>
-  </MenuProvider>
+  <div className={classNames(styles.mainContainer)}>
+    <BrowserRouter>
+      <RoutesConfig />
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { UserProvider } from "./contexts/UserContext.tsx";
+import "./index.css";
+import { UserProvider } from "./contexts/UserContext";
+import { MenuProvider } from "./contexts/MenuContext.tsx";
+import { ScrollProvider } from "./contexts/ScrollContext.tsx";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
+// import { CartProvider } from "./contexts/CartContext";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <UserProvider>
-      <App />
+      <MenuProvider>
+        <ScrollProvider>
+          <FavoritesProvider>
+            {/* <CartProvider> */}
+            <App />
+            {/* </CartProvider> */}
+          </FavoritesProvider>
+        </ScrollProvider>
+      </MenuProvider>
     </UserProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
