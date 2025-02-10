21f6025ad030ebd3167ff8149a930d114f2d59cb
Hyde <muzika.senpai@gmail.com>
2025-02-10 11:44:18 +0000

    feat: change menu to use Usercontext instead of getAuth method, add loading state and userID to Usercontext

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -9,15 +9,16 @@ import { FaPhone } from "react-icons/fa";
 import styles from "./styles.module.css";
 import { useMenuContext } from "../../contexts/MenuContext";
 import { useLogout } from "../../hooks/useLogOut";
-import { useAuth } from "../../hooks/useAuth";
+
 import { Link, useLocation } from "react-router-dom"; // Import useLocation
 import { RiAccountCircle2Fill } from "react-icons/ri";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
 const Menu = () => {
   const { isMenuOpen, setIsMenuOpen } = useMenuContext();
   const { logout, error, loading } = useLogout();
-  const { user } = useAuth();
+  const { currentUser } = useContext(UserContext);
   const location = useLocation(); // Use useLocation hook
 
   // Close menu on path change
@@ -62,7 +63,7 @@ const Menu = () => {
                 <HiHome className={classNames(styles.icons)} /> Home
               </li>
             </Link>
-            {user ? (
+            {currentUser ? (
               <Link to="/account" className={styles.navLink}>
                 <li>
                   <MdAccountCircle className={classNames(styles.icons)} />
@@ -78,10 +79,14 @@ const Menu = () => {
                 </li>
               </Link>
             )}
-            <li>
-              <MdFavorite className={classNames(styles.icons)} />
-              Favorites
-            </li>
+            {currentUser ? (
+              <Link to="/favorites" className={styles.navLink}>
+                <li>
+                  <MdFavorite className={classNames(styles.icons)} />
+                  Favorites
+                </li>
+              </Link>
+            ) : null}
             <li>
               <FaShoppingCart className={classNames(styles.icons)} />
               Cart
@@ -98,7 +103,7 @@ const Menu = () => {
               <FaPhone className={classNames(styles.icons)} />
               Contact Us
             </li>
-            {user ? (
+            {currentUser ? (
               <li onClick={logout}>
                 <FiLogOut className={classNames(styles.icons, styles.logout)} />
                 {loading ? "Logging out..." : "Logout"}
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -5,11 +5,15 @@ import { auth } from "../firebase/firebaseConfig";
 type UserContextType = {
   currentUser: User | null;
   setCurrentUser: (user: User | null) => void;
+  loading: boolean;
+  userID: string | null;
 };
 
 const defaultUserContextValue: UserContextType = {
   currentUser: null,
   setCurrentUser: () => null,
+  loading: true,
+  userID: null,
 };
 
 export const UserContext = createContext<UserContextType>(
@@ -18,10 +22,12 @@ export const UserContext = createContext<UserContextType>(
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
       setCurrentUser(user);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
@@ -31,6 +37,8 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     setCurrentUser: (user: User | null) => {
       setCurrentUser(user);
     },
+    loading,
+    userID: currentUser?.uid || null,
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
