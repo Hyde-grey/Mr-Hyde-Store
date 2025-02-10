64ae2bc2e2ca048b7745fb0d94c30ed81bce7c9d
Hyde <muzika.senpai@gmail.com>
2025-02-10 12:24:50 +0000

    feat: add suspense to mainCanvas

--- src/components/MainCanvas/MainCanvas.tsx
+++ src/components/MainCanvas/MainCanvas.tsx
@@ -2,18 +2,21 @@ import { OrbitControls, ScrollControls, Stars } from "@react-three/drei";
 import { Canvas, Vector3 } from "@react-three/fiber";
 import { ScrollUpdater } from "../ScrollUpdater";
 import useScreenSize from "../../hooks/useScreenSize";
+import { Suspense } from "react";
 
 export type MainCanvasProps = {
   children: React.ReactNode;
   numberOfPages: number;
   cameraPosition?: Vector3 | [number, number, number];
 };
+
 const MainCanvas = ({
   children,
   numberOfPages,
   cameraPosition,
 }: MainCanvasProps) => {
   const { isMobile } = useScreenSize();
+
   return (
     <Canvas camera={{ position: cameraPosition, fov: 20 }}>
       {/* @ts-expect-error mismatch library types */}
@@ -22,13 +25,15 @@ const MainCanvas = ({
       <directionalLight position={[2, 1, 4]} intensity={2.6} />
       {/* @ts-expect-error mismatch library types */}
       <pointLight position={[1, 4, -2]} intensity={54} />
-      {isMobile ? null : <OrbitControls enableZoom={false} />}
-      <ScrollControls pages={numberOfPages} enabled>
-        <Stars />
-        {/* TODO: Find a better way to implement scroll event listener as useFrame cannot be used outside of Canvas */}
-        <ScrollUpdater />
-        {children}
-      </ScrollControls>
+      {!isMobile && <OrbitControls enableZoom={false} />}
+      <Suspense fallback={null}>
+        <ScrollControls pages={numberOfPages} damping={0.1}>
+          <Stars />
+          {/* TODO: Find a better way to implement scroll event listener as useFrame cannot be used outside of Canvas */}
+          <ScrollUpdater />
+          {children}
+        </ScrollControls>
+      </Suspense>
     </Canvas>
   );
 };
