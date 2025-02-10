891690f6a7c9f687255bd2972983bb46582d654d
Hyde <muzika.senpai@gmail.com>
2025-02-10 12:27:55 +0000

    feat: add favorites features to productCard component

--- src/components/productCard/productCard.tsx
+++ src/components/productCard/productCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, memo, useContext } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/swiper-bundle.css";
@@ -6,75 +6,103 @@ import styles from "./productCard.module.css";
 import { Product } from "../../hooks/useGetCollections";
 import Button from "../button/Button";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+// import { CartContext } from "../../contexts/CartContext";
 
-const ProductCard = ({ product }: { product: Product }) => {
-  const { imageUrls, name, description, price, sizes } = product;
-  const defaultSize = sizes.length > 0 ? sizes[0].size : "";
-  const [selectedSize, setSelectedSize] = useState(defaultSize);
-  const [isFavorite, setIsFavorite] = useState(false);
-
-  const handleSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedSize(event.target.value);
-  };
-
-  const handleHeartClick = () => {
-    setIsFavorite(!isFavorite);
-    console.log(product.id);
-  };
-
-  return (
-    <div className={styles.productCardContainer}>
-      <div className={styles.productCardImage}>
-        <Swiper
-          spaceBetween={30}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Pagination]}
-          className={styles.swiper}
-        >
-          {imageUrls.map((imageUrl, index) => (
-            <SwiperSlide key={index}>
-              <img src={imageUrl} alt={name} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <button className={styles.invisibleButton} onClick={handleHeartClick}>
-          {isFavorite ? (
-            <FaHeart className={styles.favoriteButton} />
-          ) : (
-            <FaRegHeart className={styles.favoriteButton} />
-          )}
-        </button>
-      </div>
-
-      <div className={styles.productCardInfo}>
-        <div className={styles.productCardInfoTitle}>
-          <h3>{name}</h3>
-          <p>{description}</p>
-        </div>
-
-        <div className={styles.productCardPriceContainer}>
-          <div className={styles.productCardInfoSizes}>
-            <p>Sizes :</p>
-            <select value={selectedSize} onChange={handleSizeChange}>
-              {sizes.map((size, index) => (
-                <option key={index} value={size.size}>
-                  {size.size}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <h4>{price} £</h4>
-        </div>
-
-        <div className={styles.productCardButton}>
-          <Button buttonType="submit">Add to cart</Button>
-        </div>
-      </div>
-    </div>
-  );
+type ProductCardProps = {
+  product: Product;
+  favorites: number[];
+  setFavorites: (favorites: number[]) => void;
 };
+
+const ProductCard = memo(
+  ({ product, favorites, setFavorites }: ProductCardProps) => {
+    const { imageUrls, name, description, price, sizes, id } = product;
+    // const { addToCart } = useContext(CartContext);
+    const [selectedSize, setSelectedSize] = useState<string>(
+      sizes.length > 0 ? sizes[0].size.toString() : ""
+    );
+
+    const handleFavoriteClick = (e: React.MouseEvent) => {
+      e.preventDefault();
+      if (favorites.includes(id)) {
+        setFavorites(favorites.filter((favId) => favId !== id));
+      } else {
+        setFavorites([...favorites, id]);
+      }
+    };
+
+    const handleAddToCart = () => {
+      if (!selectedSize) {
+        console.error("No size selected");
+        return;
+      }
+
+      console.log("Adding to cart:", { id, selectedSize });
+      // addToCart(id, selectedSize);
+      console.log("Added to cart");
+    };
+
+    const handleSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setSelectedSize(event.target.value);
+    };
+
+    return (
+      <div className={styles.productCardContainer}>
+        <div className={styles.productCardImage}>
+          <Swiper
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className={styles.swiper}
+          >
+            {imageUrls.map((imageUrl, index) => (
+              <SwiperSlide key={index}>
+                <img src={imageUrl} alt={name} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <button
+            type="button"
+            className={styles.invisibleButton}
+            onClick={handleFavoriteClick}
+          >
+            {favorites.includes(id) ? (
+              <FaHeart className={styles.favoriteButton} />
+            ) : (
+              <FaRegHeart className={styles.favoriteButton} />
+            )}
+          </button>
+        </div>
+
+        <div className={styles.productCardInfo}>
+          <div className={styles.productCardInfoTitle}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+          </div>
+          <div className={styles.productCardPriceContainer}>
+            <div className={styles.productCardInfoSizes}>
+              <p>Sizes :</p>
+              <select value={selectedSize} onChange={handleSizeChange}>
+                {sizes.map((size, index) => (
+                  <option key={index} value={size.size}>
+                    {size.size}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <h4>{price} £</h4>
+          </div>
+          <div className={styles.productCardButton}>
+            <Button buttonType="submit" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
 
 export default ProductCard;
