cd63b08421b2db14541503d28b826681821316f0
Hyde <muzika.senpai@gmail.com>
2025-02-20 19:46:05 +0000

    feat(shop): enhance shop page with consistent styling and improved transitions

--- src/pages/shop/shophtmlLayout.tsx
+++ src/pages/shop/shophtmlLayout.tsx
@@ -1,12 +1,12 @@
 import { Scroll, useScroll } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-
+import classNames from "classnames";
 import ShopCollectionSection from "../../components/shopCollectionSection/shopCollectionsection";
 import { Collection, Product } from "../../hooks/useGetCollections";
 import useScreenSize from "../../hooks/useScreenSize";
 
 import styles from "./shop.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { memo } from "react";
 
 const ShopHtmlLayout = memo(
@@ -23,23 +23,31 @@ const ShopHtmlLayout = memo(
   }) => {
     const scroll = useScroll();
     const { isMobile } = useScreenSize();
-    const [fontSize, setFontSize] = useState(0);
+    const [fontSize, setFontSize] = useState<number | null>(null);
+    const hasRendered = useRef(false);
 
     useFrame(() => {
-      if (isMobile) {
-        const scrollY = scroll.offset;
-        setFontSize(3 - scrollY * 10);
-      } else {
-        const scrollY = scroll.offset;
-        setFontSize(10 - scrollY * 55);
-      }
+      const scrollY = scroll.offset;
+      const newSize = isMobile ? 3 - scrollY * 10 : 10 - scrollY * 55;
+      setFontSize(newSize);
+      hasRendered.current = true;
     });
 
     return (
       <Scroll html>
         <div className={styles.shopContainer}>
           <div className={styles.shopHero}>
-            <h1 style={{ fontSize: `${fontSize}rem` }}>Collections</h1>
+            <h1
+              className={classNames(styles.title, {
+                [styles.titleMobile]: isMobile,
+                [styles.titleDesktop]: !isMobile,
+              })}
+              style={
+                hasRendered.current ? { fontSize: `${fontSize}rem` } : undefined
+              }
+            >
+              Collections
+            </h1>
           </div>
           <ShopCollectionSection
             collections={collections}
