e3a539f06d14d52d8d2769e38ffb593a5a412688
Hyde <muzika.senpai@gmail.com>
2025-02-25 19:00:37 +0000

    chore: update app container structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import RoutesConfig from "./routes";
 
 import classNames from "classnames";
 import styles from "./components/MainCanvas/MainCanvas.module.css";
+import { AnimationProvider } from "./context/AnimationContext";
 
 const App = () => (
-  <div className={classNames(styles.mainContainer)}>
-    <BrowserRouter>
-      <RoutesConfig />
-    </BrowserRouter>
-  </div>
+  <AnimationProvider>
+    <div className={classNames(styles.mainContainer)}>
+      <BrowserRouter>
+        <RoutesConfig />
+      </BrowserRouter>
+    </div>
+  </AnimationProvider>
 );
 
 export default App;
