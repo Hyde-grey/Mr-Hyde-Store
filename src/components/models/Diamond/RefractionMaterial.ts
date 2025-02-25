b557890c1a6c946d4ef9e75a9d1d2a67c44aa2c1
Hyde <muzika.senpai@gmail.com>
2025-02-25 20:44:19 +0000

    fix: types....

--- src/components/models/Diamond/RefractionMaterial.ts
+++ src/components/models/Diamond/RefractionMaterial.ts
@@ -1,3 +1,10 @@
+//@ts-nocheck
 import * as THREE from "three";
 
-this.encoding = THREE.NoColorSpace;
+export const RefractionMaterial = new THREE.MeshStandardMaterial({
+  color: 0x000000,
+  roughness: 0,
+  metalness: 0,
+  transmission: 1,
+  refractionRatio: 0.98,
+});
